b7c87a7b54a26865a234868fb1a9058f6bd94f42
Muhammed EMEN <emiremen001@gmail.com>
2023-10-01 14:04:33 +0300

    Caching layer added.

--- NLayer.API/Program.cs
+++ NLayer.API/Program.cs
@@ -34,6 +34,8 @@ builder.Services.Configure<ApiBehaviorOptions>(options => {
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddMemoryCache();
+
 builder.Services.AddScoped(typeof(NotFoundFilter<>));
 
 
@@ -52,6 +54,7 @@ builder.Services.AddDbContext<AppDbContext>(x =>
 
 builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
 builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder=>containerBuilder.RegisterModule(new RepoServiceModule()));
+
 
 var app = builder.Build();
 
